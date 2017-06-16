fbfbdb93534867aea617aef74d88826927d88ec3
Charles Fourneau <charles.fourneau@gmail.com>
2017-06-16 20:49:36 +0200

    Initial commit
    LiveDNS record update is fully functionnal.

---
+++ gandilf.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import json
+from argparse import ArgumentParser
+from configparser import ConfigParser
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import logging
+
+# TODO: move to config file
+URL_IP_EXTERNAL = [
+    "http://ifconfig.me/ip",
+    "http://ipecho.net/plain",
+    "http://myexternalip.com/raw"]
+
+def get_external_ip(url_list, index=0):
+    """ get the external IP address by querying web providers """
+    try:
+        response = urlopen(url_list[index], None, 3)
+    except URLError as e:
+        #on error, try the next url
+        ip = get_external_ip(url_list, index + 1)
+    else:
+        data = response.read().decode('utf-8')
+        ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', data)[0]
+    return ip
+
+
+class RecordManager(object):
+    """ manage (create/update) DNS records from config_file """
+    
+    def __init__(self):
+        self.config = None
+
+    def load_config(self, config_file):
+        self.config = ConfigParser()
+        self.config.default_section = "general"
+        if os.path.isfile(config_file) and os.access(config_file, os.R_OK):
+            self.config.read(config_file)
+
+    def _get_record(self, url, headers):
+        try:
+            response = urlopen(Request(url, headers=headers))
+        except HTTPError as e:
+            # we have to handle http return codes in the 400-599 range (errors)
+            return None
+        if (response.getcode() != 200):
+            return None
+        encoding = response.info().get_content_charset('utf-8')
+        return json.loads(response.read().decode(encoding))
+
+    def update_records(self):
+        ip = get_external_ip(URL_IP_EXTERNAL)
+        for r in self.config.sections():
+            headers = { 'Content-Type': 'application/json',
+                        'X-Api-Key': self.config[r]["api_key"]}
+            logging.info("Record {} ({}) for domain {}".format(self.config[r]["name"],
+                                                               self.config[r]["type"],
+                                                               self.config[r]["domain"]))
+            url = '{}domains/{}/records/{}/{}'.format(self.config[r]["api"],
+                                                      self.config[r]["domain"],
+                                                      self.config[r]["name"],
+                                                      self.config[r]["type"])
+            data = {'rrset_ttl': self.config[r]["ttl"],
+                    'rrset_values': [ip]}
+            current_record = self._get_record(url, headers)
+            if current_record is None:
+                logging.info(" Record does not exist. Let's create it...")
+                method = 'POST'
+            else:
+                if current_record['rrset_values'][0] == ip:
+                    logging.info(" No IP change. Nothing to do...")
+                    continue
+                logging.info(" IP change detected. Updating...")
+                method = 'PUT'
+            json_data = json.dumps(data).encode('utf-8')
+            req = Request(url, data=json_data, headers=headers, method=method)
+            try:
+                response = urlopen(req)
+            except HTTPError as e:
+                # something has gone wrong
+                logging.info(" Record update failed with error code: {}".format(e.code))
+                continue
+            if response.getcode() != 201:
+                logging.info(" Record update failed with status code: {}".format(response.getcode()))
+                continue
+            logging.info(" Zone record updated succesfuly")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Update Gandi DNS records.')
+    parser.add_argument('-c', '--config-file', help="configuration file", dest='config_file', required=True)
+    parser.add_argument('-v', '--verbose', help="increase output verbosity", action='store_true')
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    rm = RecordManager()
+    rm.load_config(args.config_file)
+    rm.update_records()
